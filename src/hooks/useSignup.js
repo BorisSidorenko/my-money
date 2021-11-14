c0107739b713b494c0bb70ccde25ae4cb7414f96
Boris <boris.m.sidorenko@gmail.com>
2021-11-14 15:12:24 +0300

    added useLogout hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,23 +1,26 @@
 import { useState } from "react"
 import { projectAuth } from '../firebase/config';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export const useSignup = () => {
     const [error, setError] = useState(null);
     const [isPending, setIsPending] = useState(false);
+    const { dispatch } = useAuthContext();
 
     const signup = async (email, password, displayName) => {
         setError(null);
         setIsPending(true);
 
         try {
-            const res = await projectAuth.createUserWithEmailAndPassword(email, password);
-            console.log(res.user);
+            const res = await projectAuth.createUserWithEmailAndPassword(email, password);            
 
             if (!res) {
                 throw new Error('Could not complete signup');
             }
 
             await res.user.updateProfile({ displayName });
+
+            dispatch({ type: 'LOGIN', payload: res.user });
 
             setIsPending(false);
             setError(null);
