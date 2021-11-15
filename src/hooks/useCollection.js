8efe1a9ad9ec4ec940047e6660ae06077716ed2f
Boris <boris.m.sidorenko@gmail.com>
2021-11-15 16:33:31 +0300

    added new custom hook useCollection and use it on home page to display all transactions

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { projectFireStore } from '../firebase/config';
+
+export const useCollection = (collection) => {
+    const [documents, setDocuments] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        let ref = projectFireStore.collection(collection);
+        const unsubscribe = ref.onSnapshot((snapshot) => {
+            let results = [];
+            snapshot.docs.forEach(doc => {
+                results.push({ ...doc.data(), id: doc.id })
+            })
+
+            setDocuments(results);
+            setError(null);
+        }, (error) => {
+            console.log(error.message);
+            setError('Could not fetch the data');
+        });
+
+        return () => unsubscribe();
+    }, [collection]);
+
+    return { documents, error };
+}
