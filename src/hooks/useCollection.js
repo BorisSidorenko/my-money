de271ea76627b1bbf21d727cea05534fed0e1fe1
Boris <boris.m.sidorenko@gmail.com>
2021-11-15 17:43:23 +0300

    sorted the transactions

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,17 +1,22 @@
 import { useState, useEffect, useRef } from "react";
 import { projectFireStore } from '../firebase/config';
 
-export const useCollection = (collection, _query) => {
+export const useCollection = (collection, _query, _orderBy) => {
     const [documents, setDocuments] = useState(null);
     const [error, setError] = useState(null);
 
     const query = useRef(_query).current;
+    const orderBy = useRef(_orderBy).current;
 
     useEffect(() => {
         let ref = projectFireStore.collection(collection);
 
         if (query)  {
             ref = ref.where(...query);
+        }
+
+        if (orderBy) {
+            ref = ref.orderBy(...orderBy);
         }
         
         const unsubscribe = ref.onSnapshot((snapshot) => {
@@ -28,7 +33,7 @@ export const useCollection = (collection, _query) => {
         });
 
         return () => unsubscribe();
-    }, [collection, query]);
+    }, [collection, query, orderBy]);
 
     return { documents, error };
 }
