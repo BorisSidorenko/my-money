48584a72283e15ffc9214892d724352bee84f55b
Boris <boris.m.sidorenko@gmail.com>
2021-11-15 17:56:02 +0300

    added delete button on each transaction

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -11,11 +11,13 @@ let initialState = {
 const firestoreReducer = (state, action) => {
     switch (action.type) {
         case 'IS_PENDING':
-            return { isPending: true, document: null, success: false, error: null }
+            return { ...initialState, isPending: true, error: null }
         case 'ADDED_DOCUMENT':
-            return { isPending: false, document: action.payload, success: true, error: null }
+            return { document: action.payload, isPending: false, success: true, error: null }
+        case 'DELETED_DOCUMENT':
+            return { ...initialState, success: true }
         case 'ERROR':
-            return { isPending: false, document: null, success: false, error: action.payload }
+            return { ...initialState, success: false, error: action.payload }
         default:
             return state;
     }
@@ -40,13 +42,20 @@ export const useFirestore = (collection) => {
             const createdAt = timestamp.fromDate(new Date());
             const addedDocument = await ref.add({ ...doc, createdAt });
             dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument });
-        } catch (err) {
-            dispatch({ type: 'ERROR', payload: err.message });
+        } catch (error) {
+            dispatchIfNotCancelled({ type: 'ERROR', payload: error.message });
         }
     }
 
     const deleteDocument = async (id) => {
+        dispatch({ type: 'IS_PENDING' });
 
+        try {
+            await ref.doc(id).delete();
+            dispatchIfNotCancelled({ type: 'DELETED_DOCUMENT' });
+        } catch (error) {
+            dispatchIfNotCancelled({ type: 'ERROR', payload: error.message });
+        }
     }
 
     useEffect(() => {
